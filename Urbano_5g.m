849a568ff1ff68fc56a23aac5a398b926666f145
nicoVascon <105593026+nicoVascon@users.noreply.github.com>
2023-06-08 19:54:35 +0100

    Initial Commit: Without Test

--- Urbano_5g.m
+++ Urbano_5g.m
@@ -270,19 +270,19 @@ p_mc.FeedDiameter=1.3e-3;
 % [fm,~,t10] = pattern(p_mc,2.45e9,0,0:360); 
 % polarpattern(t10,fm);
 
-figure(2);
-[fmt,~,t10] = pattern(p_triang,2.60e9,0,0:360); 
-[fmr,~,t11] = pattern(p_rect,2.60e9,0,0:360); 
-[fmc,~,t12] = pattern(p_Circle,2.60e9,0,0:360); 
-[fm,~,t10] = pattern(p_mc,2.60e9,0,0:360);
-
-p=polarpattern(t10,fmt,t10,fmr,t10,fmc,t10,fm);
-legend('Antena triangular', 'Antena rectangular', 'Antena circular','Antena rect + triang');
-title('Polar pattern of antennas');
-
-figure; 
-pattern(p_triang,2.60e9);
-title('Radiation diagram antena triangular');
+% figure(2);
+% [fmt,~,t10] = pattern(p_triang,2.60e9,0,0:360); 
+% [fmr,~,t11] = pattern(p_rect,2.60e9,0,0:360); 
+% [fmc,~,t12] = pattern(p_Circle,2.60e9,0,0:360); 
+% [fm,~,t10] = pattern(p_mc,2.60e9,0,0:360);
+% 
+% p=polarpattern(t10,fmt,t10,fmr,t10,fmc,t10,fm);
+% legend('Antena triangular', 'Antena rectangular', 'Antena circular','Antena rect + triang');
+% title('Polar pattern of antennas');
+% 
+% figure; 
+% pattern(p_triang,2.60e9);
+% title('Radiation diagram antena triangular');
 
 
 %%
@@ -1420,4 +1420,174 @@ legendTitle = "RSSI" + newline + "(dBm)";                             % plot leg
 plot(pd, "LegendTitle", legendTitle, "Colormap", 'turbo');            % plot points
 contour(pd, "LegendTitle", legendTitle, "Colormap", 'turbo');         % plot countour
 
+%% SINR PARA ARRAY ANTENAS 8X8 com Canais diferentes por Célula
+% Array retangular de 8 por 8 antenas
+% E SINR
+%rx = rxsite('Name','Casa carlos', ...
+%       'Latitude',39.74120, ...
+%       'Longitude',-8.81476)
+%show(rx)
 
+% -8.807632630982194 -------------
+centerSite = txsite('Name','MathWorks Glasgow', ...
+    'Latitude',39.74422,...
+    'Longitude',-8.80763);
+
+% ------------- Criar as antenas adjacentes -------------
+numCellSites = 7;
+siteDistances = zeros(1,numCellSites);
+siteAngles = zeros(1,numCellSites);
+
+% Definir distance e angulo entre antenas centrais
+isd = 1500; % Inter-site distance
+siteDistances(2:7) = isd;
+siteAngles(2:7) = 30:60:360;
+
+% Definir distancia e angulo entre antenas a uma distancia media
+%siteDistances(8:13) = 2*isd*cosd(30);
+%siteAngles(8:13) = 0:60:300;
+
+% Definir distancia e angulo entre antenas a uma distancia alta
+
+%siteDistances(14:19) = 2*isd;
+%siteAngles(14:19) = 30:60:360;
+
+% ------------- Definir parametros das células -------------
+
+% Initialize arrays for cell transmitter parameters
+numCells = numCellSites*3;
+cellLats = zeros(1,numCells);
+cellLons = zeros(1,numCells);
+cellNames = strings(1,numCells);
+cellAngles = zeros(1,numCells);
+
+% Define cell sector angles
+cellSectorAngles = [30 150 270];
+
+% For each cell site location, populate data for each cell transmitter
+cellInd = 1;
+for siteInd = 1:numCellSites
+    % Compute site location using distance and angle from center site
+    [cellLat,cellLon] = location(centerSite, siteDistances(siteInd), siteAngles(siteInd));
+    
+    % Assign values for each cell
+    for cellSectorAngle = cellSectorAngles
+        cellNames(cellInd) = "Cell " + cellInd;
+        cellLats(cellInd) = cellLat;
+        cellLons(cellInd) = cellLon;
+        cellAngles(cellInd) = cellSectorAngle;
+        cellInd = cellInd + 1;
+    end
+end
+
+% ------------- Criar os transmissores -------------
+
+% Define transmitter parameters using Table 8-2 (b) of Report ITU-R M.[IMT-2020.EVAL]
+fq = 2.6e9; % Carrier frequency (4 GHz) for Dense Urban-eMBB
+fqs = [2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9, ...
+        2.5825e9, 2.520e9, 2.640e9];
+antHeight = 25; % m
+txPowerDBm = 44; % Total transmit power in dBm
+txPower = 10.^((txPowerDBm-30)/10); % Convert dBm to W
+
+% Create cell transmitter sites
+txs = txsite('Name',cellNames, ...
+    'Latitude',cellLats, ...
+    'Longitude',cellLons, ...
+    'AntennaAngle',cellAngles, ...
+    'AntennaHeight',antHeight, ...
+    'TransmitterFrequency',fqs, ...
+    'TransmitterPower',txPower);
+%%
+% Define array size
+nrow = 8;
+ncol = 8;
+
+% Define element spacing
+lambda = physconst('lightspeed')/fq;
+drow = lambda/2;
+dcol = lambda/2;
+
+% Define taper to reduce sidelobes 
+dBdown = 30;
+taperz = chebwin(nrow,dBdown);
+tapery = chebwin(ncol,dBdown);
+tap = taperz*tapery.'; % Multiply vector tapers to get 8-by-8 taper values
+
+% Create 8-by-8 antenna array
+cellAntenna = phased.URA('Size',[nrow ncol], ...
+    'Element',p_triang, ...
+    'ElementSpacing',[drow dcol], ...
+    'Taper',tap, ...
+    'ArrayNormal','x');
+    
+% Display radiation pattern
+% f = figure(5);
+% pattern(cellAntenna,fq);
+% title('Diagrama radiacao antena no array');
+
+% Mostrar mapa oara array antenas 8 por 8
+% Launch Site Viewer
+viewer = siteviewer("Name","SINR para array 8x8 antenas","Basemap","openstreetmap","Buildings",mapa_osm);
+viewer.Basemap = 'topographic';
+% Show sites on a map
+show(txs);
+
+
+%cellAntenna.Element =p_triang;
+
+% Assign the antenna array for each cell transmitter, and apply downtilt.
+% Without downtilt, pattern is too narrow for transmitter vicinity.
+downtilt = 15;
+for tx = txs
+    tx.Antenna = cellAntenna;
+    tx.AntennaAngle = [tx.AntennaAngle; -downtilt];
+end
+
+% Display SINR map
+if isvalid(f)
+    close(f)
+end
+%%
+sinr(txs,'longley-rice', ...
+    'ReceiverGain',rxGain, ...
+    'ReceiverAntennaHeight',rxAntennaHeight, ...
+    'ReceiverNoisePower',rxNoisePower, ...    
+    'MaxRange',isd, ...
+    'Resolution',isd/20)
+
+
+
+
+% % Display SINR map
+% if isvalid(f)
+%     close(f)
+% end
+% sinr(txs,'longley-rice',...
+%     'ReceiverGain',rxGain, ...
+%     'ReceiverAntennaHeight',rxAntennaHeight, ...
+%     'ReceiverNoisePower',rxNoisePower, ...    
+%     'MaxRange',isd, ...
+%     'Resolution',isd/20)
+
+% Sumario
+%This example shows how to construct a 5G urban macro-cell test environment consisting of a hexagonal network of 19 cell sites, 
+%each containing 3 sectored cells. The signal-to-interference-plus-noise ratio (SINR) is visualized on a map for different antennas. 
+%The following observations are made:
+
+%A rectangular antenna array can provide greater directionality and therefore peak SINR values than use of a single antenna element.
+
+%The outward-facing lobes on the perimeter of the SINR map represent areas where less interference occurs. A more realistic modelling 
+%technique would be to replicate, or wrap around, cell sites to expand the geometry so that perimeter areas experience similar interference as interior areas.
+
+%Using a rectangular antenna array, a propagation model that estimates increased path loss also results in higher SINR values due to less interference.
+
+%Two antenna elements are tried in the antenna array: an equation-based element using Phased Array System Toolbox and a patch antenna element using Antenna Toolbox. 
+%These produce similar SINR maps.
+
+% Antenas direcionais
